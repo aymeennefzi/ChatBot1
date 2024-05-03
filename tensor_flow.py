bde127d8bec5d90a89dc973e61fd3e7319d119f6
Aymen Nefzi <aymennefzi67@gmail.com>
2024-05-03 01:54:02 +0100

    add dependency11

--- tensor_flow.py
+++ tensor_flow.py
@@ -13,7 +13,7 @@ stemmer = StemmerFactory().create_stemmer()
 tf.compat.v1.disable_v2_behavior()
 tf.compat.v1.disable_eager_execution()
 
-# aa1
+# aa11
 class TensorFlow(object):
 
     def __init__(self, intents):
